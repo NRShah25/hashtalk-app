ddaa6f2db1ba084bc39720077b27e938bab0785b
Vasqjr <93558391+Vasqjr@users.noreply.github.com>
2023-09-26 20:13:37 -0400

    ServerBar Addition
    Server Bar is now apart of the UI, currently only moves by dragging mouse among bottom of all the placeholder photos. Looking to add mouse wheel capabilities to the function.

---
+++ app/(main)/(routes)/ServerBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './ServerBar.css';
+
+const ServerBar = () => {
+    // Array of image URLs as example
+    const images = [
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+    ];
+
+    return (
+        <div className="serverbar">
+            {images.map((image, index) => (
+                <img key={index} src={image} alt="" className="serverbar__image" />
+            ))}
+        </div>
+    );
+};
+
+export default ServerBar
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,12 +1,21 @@
 import Sidebar from "./Sidebar";
 import './Home.css';
 import Chat from "./Chat"
+import ServerBar from "./ServerBar"
 
   function Home() {
     return (
         <div className='Home'>
+            <header>
+                <ServerBar/>
+            </header>
             <Sidebar/>
-            <Chat />
+            <Chat/>
+
+
+
+
+
         </div>
 
     )
