20de6ed2c7be1c05aaa9ac545c577e8f4f04eb67
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-01 16:06:46 -0400

    Update navigation-item.tsx

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -26,7 +26,7 @@ export const NavigationItem = ({
 
   return (
     <ActionTooltip
-      side="right"
+      side="bottom"
       align="center"
       label={name}
     >
@@ -35,12 +35,12 @@ export const NavigationItem = ({
         className="group relative flex items-center"
       >
         <div className={cn(
-          "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
-          params?.serverId !== id && "group-hover:h-[20px]",
-          params?.serverId === id ? "h-[36px]" : "h-[8px]"
+          "absolute bottom-0 bg-primary rounded-t-full transition-all h-[4px]",
+          params?.serverId !== id && "group-hover:w-[20px]",
+          params?.serverId === id ? "w-[36px]" : "w-[8px]"
         )} />
         <div className={cn(
-          "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
+          "relative group flex mx-3 h-[48px] w-[48px] rounded-[32px] group-hover:rounded-[16px] transition-all overflow-hidden",
           params?.serverId === id && "bg-primary/10 text-primary rounded-[16px]"
         )}>
           <Image
