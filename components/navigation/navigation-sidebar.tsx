feac4e4b955725d225ea9214ab1c27340270548c
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-01 16:06:47 -0400

    Update navigation-sidebar.tsx

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -29,34 +29,38 @@ export const NavigationSidebar = async () => {
 
   return (
     <div
-      className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] bg-[#E3E5E8] py-3"
+      className="space-x-4 flex flex-row items-center w-full h-auto text-primary dark:bg-[#1E1F22] bg-[#E3E5E8] py-3 px-4"
     >
-      <NavigationAction />
-      <Separator
-        className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"
-      />
-      <ScrollArea className="flex-1 w-full">
-        {servers.map((server) => (
-          <div key={server.id} className="mb-4">
-            <NavigationItem
-              id={server.id}
-              name={server.name}
-              imageUrl={server.imageUrl}
-            />
-          </div>
-        ))}
-      </ScrollArea>
-      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
-        <ModeToggle />
-        <UserButton
-          afterSignOutUrl="/"
-          appearance={{
-            elements: {
-              avatarBox: "h-[48px] w-[48px]"
-            }
-          }}
-        />
+      <div className="flex items-center flex-row gap-x-4">
+        <NavigationAction />
       </div>
+      <Separator
+        className="w-[2px] h-full bg-zinc-300 dark:bg-zinc-700 rounded-md mx-auto"
+      />
+      <ScrollArea className="flex-1 h-full">
+        <div className="flex flex-row items-center gap-x-4">
+          {servers.map((server) => (
+            <div key={server.id} className="mr-4">
+              <NavigationItem
+                id={server.id}
+                name={server.name}
+                imageUrl={server.imageUrl}
+              />
+            </div>
+          ))}
+        </div>
+      </ScrollArea>
+      <div className="flex items-center flex-row gap-x-4">
+        <ModeToggle />
+      </div>
+      <UserButton
+        afterSignOutUrl="/"
+        appearance={{
+          elements: {
+            avatarBox: "h-[48px] w-[48px]"
+          }
+        }}
+      />
     </div>
-  )
-}
+  );
+};
