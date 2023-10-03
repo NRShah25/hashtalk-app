d610acdcd044495553142aab952c323ece36a381
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-02 21:55:21 -0400

    Update page.tsx

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,11 @@
 import { SignUp } from "@clerk/nextjs";
- 
+
+/**
+ * Page Component
+ * 
+ * This component renders the SignUp component from Clerk, facilitating user registration.
+ */
+
 export default function Page() {
   return <SignUp />;
 }
