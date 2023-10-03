1e45dab17b6f4b3400ae73af38a918763fc083a5
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-02 21:55:38 -0400

    Update route.ts

--- app/api/servers/[serverId]/leave/route.ts
+++ app/api/servers/[serverId]/leave/route.ts
@@ -2,46 +2,71 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function PATCH(
-    req: Request,
-    { params }: { params: { serverId: string } }
-) {
-    try {
-        const profile = await currentProfile();
+const unauthorizedResponse = (message = "Unauthorized") => new NextResponse(message, { status: 401 });
+const badRequestResponse = (message: string) => new NextResponse(message, { status: 400 });
+const internalErrorResponse = (message = "Internal Error") => new NextResponse(message, { status: 500 });
 
+/**
+ * Validates the user's profile.
+ * @returns The user's profile or null if unauthorized.
+ */
+async function getAuthenticatedProfile() {
+    const profile = await currentProfile();
+    return profile || null;
+}
+
+/**
+ * Handle the action of a user leaving a server.
+ * @param profileId - The ID of the authenticated user's profile.
+ * @param serverId - The ID of the server to leave.
+ * @returns The updated server object after the user leaves.
+ */
+async function leaveServer(profileId: string, serverId: string) {
+    return await db.server.update({
+        where: {
+            id: serverId,
+            profileId: {
+                not: profileId
+            },
+            members: {
+                some: {
+                    profileId: profileId
+                }
+            }
+        },
+        data: {
+            members: {
+                deleteMany: {
+                    profileId: profileId
+                }
+            }
+        }
+    });
+}
+
+/**
+ * Handle the PATCH request to allow a user to leave a server.
+ * @param req - The incoming request object.
+ * @param params - The parameters object containing serverId.
+ * @returns The updated server object or an error response.
+ */
+export async function PATCH(req: Request, { params }: { params: { serverId: string } }) {
+    try {
+        const profile = await getAuthenticatedProfile();
+        
         if (!profile) {
-            return new NextResponse("Unauthorized", { status: 401 });
+            return unauthorizedResponse();
         }
 
         if (!params.serverId) {
-            return new NextResponse("Server ID missing", { status: 400 });
+            return badRequestResponse("Server ID missing");
         }
 
-        const server = await db.server.update({
-            where: {
-                id: params.serverId,
-                profileId: {
-                    not: profile.id
-                },
-                members: {
-                    some: {
-                        profileId: profile.id
-                    }
-                }
-            },
-            data: {
-                members: {
-                    deleteMany: {
-                        profileId: profile.id
-                    }
-                }
-            }
-        });
-
-        return NextResponse.json(server);
+        const updatedServer = await leaveServer(profile.id, params.serverId);
+        return NextResponse.json(updatedServer);
 
     } catch (error) {
         console.log("[SERVER_ID_LEAVE]", error);
-        return new NextResponse("Internal Error", { status: 500 });
+        return internalErrorResponse();
     }
 }
