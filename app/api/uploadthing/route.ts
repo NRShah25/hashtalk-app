952744e19378a97d54b2cc4647118cb5ac47cf07
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-02 21:55:46 -0400

    Update route.ts

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,7 +1,11 @@
 import { createNextRouteHandler } from "uploadthing/next";
- 
 import { ourFileRouter } from "./core";
- 
-export const { GET, POST } = createNextRouteHandler({
+
+/**
+ * Using the createNextRouteHandler function, we set up the route handlers based on our file router configuration.
+ */
+const routeHandlers = createNextRouteHandler({
   router: ourFileRouter,
 });
+
+export const { GET, POST } = routeHandlers;
