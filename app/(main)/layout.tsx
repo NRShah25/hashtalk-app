74be0fcba397e73ce97f25e56dee7afd2ee6754b
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-02 21:55:29 -0400

    Update layout.tsx

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,20 +1,27 @@
+import { ReactNode } from 'react';
 import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
 
-const MainLayout = ({
-  children
-}: {
-  children: React.ReactNode;
-}) => {
-  return (
-    <div className="h-full">
-      <div className="w-full z-30 fixed inset-x-0">
-        <NavigationSidebar />
-      </div>
-      <main className="pt-[72px] h-full">
-        {children}
-      </main>
-    </div>
-  );
+interface MainLayoutProps {
+  children: ReactNode; 
 }
+
+/**
+ * The MainLayout component.
+ * It provides a layout structure for the main content area of the application.
+ * - At the top, it renders the `NavigationSidebar` component.
+ * - Below that, it renders any child components passed into it.
+ * 
+ * @param {MainLayoutProps} { children } - Child components passed to the layout.
+ */
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) => (
+  <div className="h-full">
+    <div className="w-full z-30 fixed inset-x-0">
+      <NavigationSidebar />
+    </div>
+    <main className="pt-[72px] h-full">
+      {children} 
+    </main>
+  </div>
+);
 
 export default MainLayout;
