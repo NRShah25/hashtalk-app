965bf2f3bce11165517d8758c4bd1ff60b7347e2
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-02 21:55:34 -0400

    Update route.ts

--- app/api/members/[memberId]/route.ts
+++ app/api/members/[memberId]/route.ts
@@ -1,29 +1,54 @@
 import { NextResponse } from "next/server";
-
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
-export async function DELETE(
-  req: Request,
-  { params }: { params: { memberId: string } }
-) {
+const unauthorizedResponse = (message = "Unauthorized") => new NextResponse(message, { status: 401 });
+const badRequestResponse = (message: string) => new NextResponse(message, { status: 400 });
+const internalErrorResponse = (message = "Internal Error") => new NextResponse(message, { status: 500 });
+
+/**
+ * Extract the serverId from the request URL.
+ * @param req - The incoming request object.
+ * @returns The serverId from the URL or null.
+ */
+const getServerIdFromRequest = (req: Request): string | null => {
+  const { searchParams } = new URL(req.url);
+  return searchParams.get("serverId");
+}
+
+/**
+ * Validate the request, ensuring a valid profile, serverId, and memberId are present.
+ * @param req - The incoming request object.
+ * @param params - The parameters object containing memberId.
+ * @returns An object containing potential errors and extracted profile and serverId.
+ */
+const validateRequest = async (req: Request, params: { memberId: string }) => {
+  const profile = await currentProfile();
+  const serverId = getServerIdFromRequest(req);
+
+  if (!profile) {
+    return { error: unauthorizedResponse() };
+  }
+  if (!serverId) {
+    return { error: badRequestResponse("Server ID missing") };
+  }
+  if (!params.memberId) {
+    return { error: badRequestResponse("Member ID missing") };
+  }
+
+  return { profile, serverId };
+}
+
+/**
+ * Handler to delete a member from a server.
+ * @param req - The incoming request object.
+ * @param params - The parameters object containing memberId.
+ * @returns The updated server object or an error response.
+ */
+export async function DELETE(req: Request, { params }: { params: { memberId: string } }) {
   try {
-    const profile = await currentProfile();
-    const { searchParams } = new URL(req.url);
-
-    const serverId = searchParams.get("serverId");
-
-    if (!profile) {
-      return new NextResponse("Unauthorized" ,{ status: 401 });
-    }
-
-    if (!serverId) {
-      return new NextResponse("Server ID missing", { status: 400 });
-    }
-
-    if (!params.memberId) {
-      return new NextResponse("Member ID missing", { status: 400 });
-    }
+    const { error, profile, serverId } = await validateRequest(req, params);
+    if (error) return error;
 
     const server = await db.server.update({
       where: {
@@ -34,53 +59,37 @@ export async function DELETE(
         members: {
           deleteMany: {
             id: params.memberId,
-            profileId: {
-              not: profile.id
-            }
+            profileId: { not: profile.id }
           }
         }
       },
       include: {
         members: {
-          include: {
-            profile: true,
-          },
-          orderBy: {
-            role: "asc",
-          }
-        },
-      },
+          include: { profile: true },
+          orderBy: { role: "asc" }
+        }
+      }
     });
 
     return NextResponse.json(server);
   } catch (error) {
     console.log("[MEMBER_ID_DELETE]", error);
-    return new NextResponse("Internal Error", { status: 500 });
+    return internalErrorResponse();
   }
 }
 
-export async function PATCH(
-  req: Request,
-  { params }: { params: { memberId: string } }
-) {
+/**
+ * Handler to update a member's role in a server.
+ * @param req - The incoming request object.
+ * @param params - The parameters object containing memberId.
+ * @returns The updated server object or an error response.
+ */
+export async function PATCH(req: Request, { params }: { params: { memberId: string } }) {
   try {
-    const profile = await currentProfile();
-    const { searchParams } = new URL(req.url);
+    const { error, profile, serverId } = await validateRequest(req, params);
+    if (error) return error;
+
     const { role } = await req.json();
-
-    const serverId = searchParams.get("serverId");
-
-    if (!profile) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
-
-    if (!serverId) {
-      return new NextResponse("Server ID missing", { status: 400 });
-    }
-
-    if (!params.memberId) {
-      return new NextResponse("Member ID missing", { status: 400 });
-    }
 
     const server = await db.server.update({
       where: {
@@ -92,24 +101,16 @@ export async function PATCH(
           update: {
             where: {
               id: params.memberId,
-              profileId: {
-                not: profile.id
-              }
+              profileId: { not: profile.id }
             },
-            data: {
-              role
-            }
+            data: { role }
           }
         }
       },
       include: {
         members: {
-          include: {
-            profile: true,
-          },
-          orderBy: {
-            role: "asc"
-          }
+          include: { profile: true },
+          orderBy: { role: "asc" }
         }
       }
     });
@@ -117,6 +118,6 @@ export async function PATCH(
     return NextResponse.json(server);
   } catch (error) {
     console.log("[MEMBERS_ID_PATCH]", error);
-    return new NextResponse("Internal Error", { status: 500 });
+    return internalErrorResponse();
   }
 }
