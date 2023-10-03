c6af6f14a76a45998fbe5c96106bcc50c770f5ab
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-02 21:55:44 -0400

    Update core.ts

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,21 +1,30 @@
 import { auth } from "@clerk/nextjs";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
- 
-const f = createUploadthing();
- 
-const handleAuth = () => {
-  const { userId } = auth();
-  if (!userId) throw new Error("Unauthorized");
-  return { userId: userId };
-}
 
+const f = createUploadthing();
+
+/**
+ * Middleware function to ensure the user is authenticated using Clerk.
+ * If the user is not authenticated, it throws an "Unauthorized" error.
+ * @returns Object containing the authenticated user's ID.
+ */
+const handleAuth = () => {
+    const { userId } = auth();
+    if (!userId) throw new Error("Unauthorized");
+    return { userId };
+};
+
+/**
+ * Define our custom file router configurations for different upload scenarios.
+ */
 export const ourFileRouter = {
-  serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
-    .middleware(() => handleAuth())
-    .onUploadComplete(() => {}),
-  messageFile: f(["image", "pdf"])
-    .middleware(() => handleAuth())
-    .onUploadComplete(() => {})
+    serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+        .middleware(handleAuth)
+        .onUploadComplete(() => {}),
+
+    messageFile: f(["image", "pdf"])
+        .middleware(handleAuth)
+        .onUploadComplete(() => {})
 } satisfies FileRouter;
- 
+
 export type OurFileRouter = typeof ourFileRouter;
