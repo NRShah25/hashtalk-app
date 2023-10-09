f5464cc49a918d23ead7fe595499f239495dd3cf
NRShah25 <103145108+NRShah25@users.noreply.github.com>
2023-10-09 16:00:55 -0400

    added text bar

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
 const ServerIdPage = () => {
     return (
-        <div>This is a placeholder!</div>
+        <div className="flex flex-col items-center justify-end h-full">
+            <div className="w-full">
+                <Textarea placeholder="Type your message here." />
+                <div className="flex justify-end">
+                    <Button>Send message</Button>
+                </div>
+            </div>
+        </div>
     );
-}
+};
 
 export default ServerIdPage;
+
