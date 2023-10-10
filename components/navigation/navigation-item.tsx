2fdde963f651534a28c3ec8780ab48f6d39d12bd
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-09 23:12:06 -0400

    Update navigation-item.tsx

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -48,11 +48,6 @@ export const NavigationItem = ({
         className="group relative flex items-center"
       >
         <div className={cn(
-          "absolute bottom-0 bg-primary rounded-t-full transition-all h-[4px]",
-          params?.serverId !== id && "group-hover:w-[20px]",
-          params?.serverId === id ? "w-[36px]" : "w-[8px]"
-        )} />
-        <div className={cn(
           "relative group flex mx-3 h-[48px] w-[48px] rounded-[32px] group-hover:rounded-[16px] transition-all overflow-hidden",
           params?.serverId === id && "bg-primary/10 text-primary rounded-[16px]"
         )}>
