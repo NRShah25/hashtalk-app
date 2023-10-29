c4c61101a610c9b27dab6d90634dff64c5d64b6c
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-29 13:29:54 -0400

    Update chat-messages.tsx

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -93,12 +93,6 @@ export const ChatMessages = ({
     return (
         <div ref={chatRef} className="flex-1 flex flex-col py-4 overflow-y-auto">
             {!hasNextPage && <div className = "flex-1" />}
-            {!hasNextPage && (
-                <ChatWelcome 
-                    type = {type}
-                    name = {name}
-                />
-            )}
             {hasNextPage && (
                 <div className = "flex justify-center">
                     {isFetchingNextPage ?(
