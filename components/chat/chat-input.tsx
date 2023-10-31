88819dde23e76bf9944555a8df308b518c437d1b
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-31 00:42:54 -0400

    Update chat-input.tsx

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -14,7 +14,7 @@ import {
 } from "@/components/ui/form";
 
 import {Input} from "@/components/ui/input";
-import { Plus } from "lucide-react";
+import { Paperclip, SendHorizonal } from "lucide-react";
 import { useModal } from "@/hooks/use-modal-store";
 import { EmojiPicker } from "../emoji-picker";
 import { useRouter } from "next/navigation";
@@ -73,19 +73,23 @@ export const ChatInput = ({
                         <FormItem>
                             <FormControl>
                                 <div className="relative p-4 pb-6">
-                                    <button type="button"
-                                    onClick={() => onOpen("messageFile", { apiUrl, query })}
-                                    className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center">
-                                        <Plus className="text-white dark:text-[#313338]"/>
-                                    </button>
                                     <Input 
                                         disabled={isLoading}
-                                        className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
+                                        className="px-3 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
                                         placeholder={`Message ${type === "conversation" ? name : "#" + name}`}
                                         autoComplete="off"
                                         {...field}
                                     />
-                                    <div className="absolute top-7 right-8">
+                                    <div className="absolute top-7 right-8 flex">
+                                        <button type="submit" 
+                                                className="h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center mr-2">
+                                                <SendHorizonal className="text-white dark:text-[#313338]"/>
+                                        </button>
+                                        <button type="button" 
+                                            onClick={() => onOpen("messageFile", { apiUrl, query })}
+                                            className="h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center mr-2">
+                                            <Paperclip className="text-white dark:text-[#313338]"/>
+                                        </button>
                                         <EmojiPicker 
                                             onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
                                         />
