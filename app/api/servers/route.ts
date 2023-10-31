9c1afcf71e66f4e13058559539dfeac36d2059bd
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-30 22:45:41 -0400

    Update route.ts

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -1,41 +1,61 @@
+/**
+ * @module route.ts
+ * @description This module provides an API route handler for creating new servers.
+ * @requires uuid
+ * @requires next/server
+ * @requires @prisma/client
+ * @requires @/lib/current-profile
+ * @requires @/lib/db
+ */
+
 import { v4 as uuidv4 } from "uuid";
 import { NextResponse } from "next/server";
 import { MemberRole } from "@prisma/client";
-
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
-export async function POST(req: Request) {
-  try {
-    const { name, imageUrl } = await req.json();
-    const profile = await currentProfile();
+/**
+ * Creates a new server with the provided details.
+ * 
+ * This function handles the POST request to create a new server.
+ * It generates a unique invite code for the server, creates a default "general" channel,
+ * and adds the logged-in user to the server as an admin.
+ * 
+ * @param {Request} req - The request object containing server details.
+ * @returns {Promise<NextResponse>} A promise that resolves to the response of the operation.
+ */
+export async function POST(req: Request): Promise<NextResponse> {
+    try {
+        const { name, imageUrl } = await req.json();
+        const profile = await currentProfile();
 
-    if (!profile) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
-
-    const server = await db.server.create({
-      data: {
-        profileId: profile.id,
-        name,
-        imageUrl,
-        inviteCode: uuidv4(),
-        channels: {
-          create: [
-            { name: "general", profileId: profile.id }
-          ]
-        },
-        members: {
-          create: [
-            { profileId: profile.id, role: MemberRole.ADMIN }
-          ]
+        if (!profile) {
+            return new NextResponse("Unauthorized", { status: 401 });
         }
-      }
-    });
 
-    return NextResponse.json(server);
-  } catch (error) {
-    console.log("[SERVERS_POST]", error);
-    return new NextResponse("Internal Error", { status: 500 });
-  }
+        const server = await db.server.create({
+            data: {
+                profileId: profile.id,
+                name,
+                imageUrl,
+                inviteCode: uuidv4(),
+                channels: {
+                    create: [
+                        { name: "general", profileId: profile.id }
+                    ]
+                },
+                members: {
+                    create: [
+                        { profileId: profile.id, role: MemberRole.ADMIN }
+                    ]
+                }
+            }
+        });
+
+        return NextResponse.json(server);
+
+    } catch (error) {
+        console.log("[SERVERS_POST]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
 }
