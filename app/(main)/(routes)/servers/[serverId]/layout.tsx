49062ff2362ecded96772f0438faf105b0c89dd1
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-30 21:31:50 -0400

    Update layout.tsx

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,17 +1,46 @@
+/**
+ * @module layout.tsx
+ * @description This module provides a layout component for displaying server-specific content, including a server sidebar.
+ * @requires @clerk/nextjs
+ * @requires next/navigation
+ * @requires @/lib/db
+ * @requires @/lib/current-profile
+ * @requires @/components/server/server-sidebar
+ */
+
 import { redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-
 import { db } from "@/lib/db";
 import { currentProfile } from "@/lib/current-profile";
 import { ServerSidebar } from "@/components/server/server-sidebar";
 
+/**
+ * Props type for the ServerIdLayout component.
+ * @typedef {Object} ServerIdLayoutProps
+ * @property {React.ReactNode} children - Child components to be rendered in the main content area.
+ * @property {Object} params - Parameters passed to the component.
+ * @property {string} params.serverId - The ID of the server.
+ */
+interface ServerIdLayoutProps {
+  children: React.ReactNode;
+  params: {
+    serverId: string;
+  };
+}
+
+/**
+ * ServerIdLayout is an async component that handles the layout for server-specific content.
+ * 
+ * It fetches the current user's profile and ensures they are a member of the specified server.
+ * The layout consists of a server sidebar on the left and a main content area on the right.
+ * 
+ * @param {ServerIdLayoutProps} props - The props for the ServerIdLayout component.
+ * @returns {Promise<JSX.Element|null>} A promise that resolves to the server layout or a redirection.
+ */
 const ServerIdLayout = async ({
   children,
   params,
-}: {
-  children: React.ReactNode;
-  params: { serverId: string };
-}) => {
+}: ServerIdLayoutProps): Promise<JSX.Element|null> => {
   const profile = await currentProfile();
 
   if (!profile) {
@@ -33,17 +62,16 @@ const ServerIdLayout = async ({
     return redirect("/");
   }
 
-  return ( 
+  return (
     <div className="h-full">
-      <div 
-      className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
         <ServerSidebar serverId={params.serverId} />
       </div>
       <main className="h-full md:pl-60">
         {children}
       </main>
     </div>
-   );
+  );
 }
- 
+
 export default ServerIdLayout;
