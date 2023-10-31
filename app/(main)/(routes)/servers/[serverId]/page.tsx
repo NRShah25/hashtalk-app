8ce4fd85dd2541c0c21af62e94b5cc8a3366d4c8
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-30 21:31:52 -0400

    Update page.tsx

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,17 +1,41 @@
+/**
+ * @module page.tsx
+ * @description This module provides a page component that redirects users to the initial channel of a specified server.
+ * @requires @clerk/nextjs
+ * @requires next/navigation
+ * @requires @/lib/db
+ * @requires @/lib/current-profile
+ */
+
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+/**
+ * Props type for the ServerIdPage component.
+ * @typedef {Object} ServerIdPageProps
+ * @property {Object} params - Parameters passed to the component.
+ * @property {string} params.serverId - The ID of the server.
+ */
 interface ServerIdPageProps {
     params: {
         serverId: string;
     }
-};
+}
 
+/**
+ * ServerIdPage is an async component that redirects users to the initial channel of a specified server.
+ * 
+ * It first checks if the user is authenticated and a member of the server.
+ * If the user is a member, it then looks for the initial channel and redirects to it.
+ * 
+ * @param {ServerIdPageProps} props - The props for the ServerIdPage component.
+ * @returns {Promise<JSX.Element|null>} A promise that resolves to a redirection or null.
+ */
 const ServerIdPage = async ({
     params
-}: ServerIdPageProps) => {
+}: ServerIdPageProps): Promise<JSX.Element|null> => {
     const profile = await currentProfile();
 
     if (!profile) {
@@ -37,7 +61,7 @@ const ServerIdPage = async ({
                 }
             }
         }
-    })
+    });
 
     const initialChannel = server?.channels[0];
 
@@ -45,7 +69,7 @@ const ServerIdPage = async ({
         return null;
     }
 
-    return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`)
+    return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
 }
 
 export default ServerIdPage;
