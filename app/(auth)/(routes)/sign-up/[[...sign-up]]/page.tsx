7579df636674a9b1c7b655c6270ea7f57d6e9cd0
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-30 21:31:40 -0400

    Update page.tsx

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,18 @@
+/**
+ * @module page.tsx
+ * @description This module provides a page component for user sign-up.
+ * @requires @clerk/nextjs
+ */
+
 import { SignUp } from "@clerk/nextjs";
- 
-export default function Page() {
+
+/**
+ * The Page component renders the SignUp component for user registration.
+ * 
+ * @returns {JSX.Element} A React component that represents the sign-up page.
+ */
+const Page = () => {
   return <SignUp />;
 }
+
+export default Page;
