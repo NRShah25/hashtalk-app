54dd81afa45c0cabd346f93bb861d99f22ee55b0
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-30 21:31:38 -0400

    Update page.tsx

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,18 @@
+/**
+ * @module page.tsx
+ * @description This module provides a page component for user sign-in.
+ * @requires @clerk/nextjs
+ */
+
 import { SignIn } from "@clerk/nextjs";
- 
-export default function Page() {
+
+/**
+ * The Page component renders the SignIn component for user authentication.
+ * 
+ * @returns {JSX.Element} A React component that represents the sign-in page.
+ */
+const Page = () => {
   return <SignIn />;
 }
+
+export default Page;
