0219bbfe6d1e7709346c4d67823646b719a48e11
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-30 22:45:37 -0400

    Update route.ts

--- app/api/servers/[serverId]/leave/route.ts
+++ app/api/servers/[serverId]/leave/route.ts
@@ -1,11 +1,30 @@
+/**
+ * @module route.ts
+ * @description This module provides an API route handler for allowing a user to leave a specified server.
+ * @requires @/lib/current-profile
+ * @requires @/lib/db
+ * @requires next/server
+ */
+
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+/**
+ * Allows a user to leave a specified server by its serverId.
+ * 
+ * This function handles the PATCH request to remove the current user's profile from the server's member list.
+ * The logged-in user must be a member of the server, but not the owner, to execute this action.
+ * 
+ * @param {Request} req - The request object.
+ * @param {Object} params - The parameters passed to the route.
+ * @param {string} params.serverId - The ID of the server.
+ * @returns {Promise<NextResponse>} A promise that resolves to the response of the operation.
+ */
 export async function PATCH(
     req: Request,
     { params }: { params: { serverId: string } }
-) {
+): Promise<NextResponse> {
     try {
         const profile = await currentProfile();
 
