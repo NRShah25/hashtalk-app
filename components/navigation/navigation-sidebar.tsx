b9045143f693c7aae9f6fd7f3539ed26d6d7e4a4
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-30 23:54:46 -0400

    Update navigation-sidebar.tsx

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -50,7 +50,7 @@ export const NavigationSidebar = async () => {
     });
 
     return (
-        <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] bg-[#E3E5E8] py-3">
+      <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] bg-[#E3E5E8] py-3">
             <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
                 <UserButton
                     afterSignOutUrl="/"
