41468cfc2843a10480734e2a56f041afa72c0b75
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-31 00:58:13 -0400

    Update server-channel.tsx

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from "@/lib/utils";
 import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
-import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
+import { Edit, Lock, MessagesSquare, Mic, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { ActionTooltip } from "../action-tooltip";
 import { ModalType, useModal } from "@/hooks/use-modal-store";
@@ -14,7 +14,7 @@ interface ServerChannelProps {
 }
 
 const iconMap = {
-    [ChannelType.TEXT]: Hash,
+    [ChannelType.TEXT]: MessagesSquare,
     [ChannelType.AUDIO]: Mic,
     [ChannelType.VIDEO]: Video,
 }
