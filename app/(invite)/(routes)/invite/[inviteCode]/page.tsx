c344b3775e410d8d4ef4a83b68dd8bd11a2c726a
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-30 21:31:44 -0400

    Update page.tsx

--- app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -1,18 +1,43 @@
+/**
+ * @module page.tsx
+ * @description This module provides a page component to handle invite codes for servers.
+ * @requires @clerk/nextjs
+ * @requires next/navigation
+ * @requires @/lib/db
+ * @requires @/lib/current-profile
+ */
+
 import { redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
 import { currentProfile } from "@/lib/current-profile";
 
+/**
+ * Props type for the InviteCodePage component.
+ * @typedef {Object} InviteCodePageProps
+ * @property {Object} params - Parameters passed to the component.
+ * @property {string} params.inviteCode - The invite code for the server.
+ */
 interface InviteCodePageProps {
   params: {
     inviteCode: string;
   };
-};
+}
 
+/**
+ * InviteCodePage is an async component that handles invite codes.
+ * 
+ * It checks if the current user's profile has already joined a server using the provided invite code.
+ * If the profile has joined, it redirects to that server's page.
+ * Otherwise, it joins the server and then redirects to that server's page.
+ * 
+ * @param {InviteCodePageProps} props - The props for the InviteCodePage component.
+ * @returns {Promise<JSX.Element|null>} A promise that resolves to a redirection or null.
+ */
 const InviteCodePage = async ({
   params
-}: InviteCodePageProps) => {
+}: InviteCodePageProps): Promise<JSX.Element|null> => {
   const profile = await currentProfile();
 
   if (!profile) {
@@ -59,5 +84,5 @@ const InviteCodePage = async ({
   
   return null;
 }
- 
+
 export default InviteCodePage;
