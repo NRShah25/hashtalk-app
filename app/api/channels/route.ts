3d14c0b693862bbdd67a47683f18aec01aff255f
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-30 21:31:59 -0400

    Update route.ts

--- app/api/channels/route.ts
+++ app/api/channels/route.ts
@@ -1,11 +1,28 @@
+/**
+ * @module route.ts
+ * @description This module provides an API route handler for creating channels within a server.
+ * @requires @/lib/current-profile
+ * @requires @prisma/client
+ * @requires next/server
+ * @requires @/lib/db
+ */
+
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { MemberRole } from "@prisma/client";
 import { NextResponse } from "next/server";
 
-export async function POST(
-    req: Request
-) {
+/**
+ * Creates a new channel within a specified server.
+ * 
+ * This function handles the POST request to create a channel.
+ * It first checks if the current user's profile is valid and if the required parameters are provided.
+ * The user must be either an ADMIN or MODERATOR of the server to create a channel.
+ * 
+ * @param {Request} req - The request object.
+ * @returns {Promise<NextResponse>} A promise that resolves to the response of the operation.
+ */
+export async function POST(req: Request): Promise<NextResponse> {
     try {
         const profile = await currentProfile();
         const { name, type } = await req.json();
