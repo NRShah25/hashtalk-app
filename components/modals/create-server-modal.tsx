a75d1b252950130ffa67f60226c017ae5f17e9ec
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-31 00:17:00 -0400

    Update create-server-modal.tsx

--- components/modals/create-server-modal.tsx
+++ components/modals/create-server-modal.tsx
@@ -74,10 +74,10 @@ export const CreateServerModal = () => {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Customize your server
+            Create Server
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500">
-            Give your server a personality with a name and an image. You can always change it later.
+            Give your server a name and an image. You can always change it later.
           </DialogDescription>
         </DialogHeader>
         <Form {...form}>
