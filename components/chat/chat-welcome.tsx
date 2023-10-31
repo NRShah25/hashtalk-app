b724149c22a6d02fade904048be6c8a36cde9d70
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-31 00:58:10 -0400

    Update chat-welcome.tsx

--- components/chat/chat-welcome.tsx
+++ components/chat/chat-welcome.tsx
@@ -1,4 +1,4 @@
-import { Hash } from "lucide-react";
+import { MessagesSquare } from "lucide-react";
 
 interface ChatWelcomeProps {
     name: string;
@@ -13,7 +13,7 @@ export const ChatWelcome = ({
         <div className="space-y-2 px-4 mb-4">
             {type === "channel" && (
                 <div className = "h-[75px] w-[75px] rounded-full bg-zinc-500 dark:bg-zinc-700 flex items-center justify-center">
-                    <Hash className="h-12 w-12 text-white" />
+                    <MessagesSquare className="h-12 w-12 text-white" />
                 </div>
             )}
             <p className="text-xl md:text-3xl font-bold">
