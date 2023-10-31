d8dba8d3b80249e2816385ff78743cf51645ff2e
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-30 22:45:44 -0400

    Update route.ts

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,7 +1,19 @@
+/**
+ * @module route.ts
+ * @description This module provides route handlers for the file upload functionality in the application.
+ * @requires uploadthing/next
+ * @requires ./core
+ */
+
 import { createNextRouteHandler } from "uploadthing/next";
- 
 import { ourFileRouter } from "./core";
- 
+
+/**
+ * Creates the Next.js route handlers for file operations.
+ * 
+ * This function generates the GET and POST route handlers for file uploads.
+ * It leverages the `ourFileRouter` configuration defined in the `core.ts` module.
+ */
 export const { GET, POST } = createNextRouteHandler({
-  router: ourFileRouter,
+    router: ourFileRouter,
 });
