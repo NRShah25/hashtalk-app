4b3caa06099c38f3f4e80541890560382d70b28c
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-30 22:45:39 -0400

    Update route.ts

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -1,12 +1,30 @@
-import { NextResponse } from "next/server";
+/**
+ * @module route.ts
+ * @description This module provides API route handlers for specific server operations.
+ * @requires next/server
+ * @requires @/lib/current-profile
+ * @requires @/lib/db
+ */
 
+import { NextResponse } from "next/server";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
+/**
+ * Deletes a specified server by its serverId.
+ * 
+ * This function handles the DELETE request to remove a server.
+ * The logged-in user must be the owner of the server to execute this action.
+ * 
+ * @param {Request} req - The request object.
+ * @param {Object} params - The parameters passed to the route.
+ * @param {string} params.serverId - The ID of the server.
+ * @returns {Promise<NextResponse>} A promise that resolves to the response of the operation.
+ */
 export async function DELETE(
-  req: Request,
-  { params }: { params: { serverId: string } }
-) {
+    req: Request,
+    { params }: { params: { serverId: string } }
+): Promise<NextResponse> {
   try {
     const profile = await currentProfile();
 
@@ -28,10 +46,21 @@ export async function DELETE(
   }
 }
 
+/**
+ * Updates the details of a specified server by its serverId.
+ * 
+ * This function handles the PATCH request to update server details.
+ * The logged-in user must be the owner of the server to execute this action.
+ * 
+ * @param {Request} req - The request object.
+ * @param {Object} params - The parameters passed to the route.
+ * @param {string} params.serverId - The ID of the server.
+ * @returns {Promise<NextResponse>} A promise that resolves to the response of the operation.
+ */
 export async function PATCH(
   req: Request,
   { params }: { params: { serverId: string } }
-) {
+): Promise<NextResponse> {
   try {
     const profile = await currentProfile();
     const { name, imageUrl } = await req.json();
