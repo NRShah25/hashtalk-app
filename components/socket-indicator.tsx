935a9a44ddf9086ac54c6ba32148d16a8d36610c
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-31 00:42:55 -0400

    Update socket-indicator.tsx

--- components/socket-indicator.tsx
+++ components/socket-indicator.tsx
@@ -1,28 +1,14 @@
 "use client";
 
 import { useSocket } from "@/components/providers/socket-provider";
-import { Badge } from "@/components/ui/badge";
 
 export const SocketIndicator = () => {
   const { isConnected } = useSocket();
 
-  if (!isConnected) {
-    return (
-      <Badge 
-        variant="outline" 
-        className="bg-yellow-600 text-white border-none"
-      >
-        Connecting
-      </Badge>
-    )
-  }
-
   return (
-    <Badge 
-      variant="outline" 
-      className="bg-emerald-600 text-white border-none"
-    >
-      Connected
-    </Badge>
+    <div 
+      className={`w-4 h-4 rounded-full ${isConnected ? "bg-emerald-600" : "bg-yellow-600"}`}
+      title={isConnected ? "Connected" : "Not Connected"}
+    ></div>
   )
 }
