f6e59a35b5c4bef64a3ab53946c7f5a7c77201d3
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-10-30 21:31:54 -0400

    Update layout.tsx

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,11 +1,33 @@
+/**
+ * @module layout.tsx
+ * @description This module provides a main layout component for the application, including a navigation sidebar.
+ * @requires @/components/navigation/navigation-sidebar
+ */
+
 import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
 
+/**
+ * Props type for the MainLayout component.
+ * @typedef {Object} MainLayoutProps
+ * @property {React.ReactNode} children - Child components to be rendered in the main content area.
+ */
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+/**
+ * MainLayout is a component that provides the main structure of the application.
+ * 
+ * The layout consists of a navigation sidebar on the left and a main content area on the right.
+ * The navigation sidebar is hidden on smaller screens.
+ * 
+ * @param {MainLayoutProps} props - The props for the MainLayout component.
+ * @returns {JSX.Element} A React component that represents the main application layout.
+ */
 const MainLayout = async ({
   children
-}: {
-  children: React.ReactNode;
-}) => {
-  return ( 
+}: MainLayoutProps): Promise<JSX.Element> => {
+  return (
     <div className="h-full">
       <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
         <NavigationSidebar />
@@ -14,7 +36,7 @@ const MainLayout = async ({
         {children}
       </main>
     </div>
-   );
+  );
 }
- 
+
 export default MainLayout;
