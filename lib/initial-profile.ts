baf1f05ca5fde2cb56c20ea4528cd303bdc13e47
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-11-13 18:26:54 -0500

    Update initial-profile.ts

--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -22,7 +22,7 @@ export const initialProfile = async () => {
   const newProfile = await db.profile.create({
     data: {
       userId: user.id,
-      name: `${user.firstName} ${user.lastName}`,
+      username: user.username as string,
       imageUrl: user.imageUrl,
       email: user.emailAddresses[0].emailAddress
     }
