5dc781388006d4937fdb4bcb9f2f366d8fee640f
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-11-13 18:27:15 -0500

    Create edit-profile-modal.tsx

---
+++ components/modals/edit-profile-modal.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import axios from "axios";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useModal } from "@/hooks/use-modal-store";
+
+const formSchema = z.object({
+  status: z.string().min(1, {
+    message: "(CHANGE LATER) Status is required."
+  })
+});
+
+export const EditProfileModal = () => {
+  const { isOpen, onClose, type, data } = useModal();
+  const router = useRouter();
+
+  const isModalOpen = isOpen && type === "editProfile";
+  const { profile } = data;
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      status: "Hello world!",
+    }
+  });
+
+  useEffect(() => {
+    if (profile) {
+      form.setValue("status", profile.username);
+    }
+  }, [profile, form]);
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/profiles/${profile?.id}`, values);
+
+      form.reset();
+      router.refresh();
+      onClose();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleClose = () => {
+    form.reset();
+    onClose();
+  }
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Edit Profile
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Description
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <FormField
+                control={form.control}
+                name="status"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel
+                      className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70"
+                    >
+                      Status
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                        placeholder="How are you feeling today?"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button variant="primary" disabled={isLoading}>
+                Save
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  )
+}
