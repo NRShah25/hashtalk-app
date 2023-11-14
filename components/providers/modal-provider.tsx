92972df161112babbccf655229ce0678cd97e8ea
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-11-13 22:23:45 -0500

    Update modal-provider.tsx

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -13,7 +13,7 @@ import { DeleteChannelModal } from "../modals/delete-channel-modal";
 import { EditChannelModal } from "../modals/edit-channel-modal";
 import { MessageFileModal } from "../modals/message-file-modal";
 import { DeleteMessageModal } from "../modals/delete-message-modal";
-import { SearchModal } from "../modals/search-modal";
+import { ExploreModal } from "../modals/explore-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -28,7 +28,7 @@ export const ModalProvider = () => {
 
   return (
     <>
-      <SearchModal />
+      <ExploreModal />
       <CreateServerModal />
       <InviteModal />
       <EditServerModal />
