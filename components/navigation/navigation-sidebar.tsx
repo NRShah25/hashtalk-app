c23c61f1bc315ea2afb0399c44ca802f1f2a92f8
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-11-26 22:40:01 -0500

    Update navigation-sidebar.tsx

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -24,6 +24,8 @@ export const NavigationSidebar = async () => {
         }
     });
 
+    const isAuthenticated = true;
+
     return (
         <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] bg-[#E3E5E8] py-3">
             <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
@@ -35,7 +37,7 @@ export const NavigationSidebar = async () => {
                         }
                     }}
                 />
-                <NavigationAction profile={profile} />
+                <NavigationAction profile={profile} isAuthenticated={isAuthenticated}/>
             </div>
             <ScrollArea className="flex-1 w-full">
                 {servers.map((server) => (
