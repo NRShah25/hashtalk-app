7ff1ccb4155d1e3b7d711e614c919f01cfa53eda
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-11-26 19:17:47 -0500

    Update route.ts

--- app/api/profiles/[profileId]/route.ts
+++ app/api/profiles/[profileId]/route.ts
@@ -1,31 +1,26 @@
-/**
- * @module route.ts
- * @description This module provides an API route handler for creating new servers.
- * @requires uuid
- * @requires next/server
- * @requires @prisma/client
- * @requires @/lib/current-profile
- * @requires @/lib/db
- */
-
+import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
-import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: { url: string | URL; }) {
     try {
-        const { profileId } = req.query;
+        const url = new URL(req.url);
+        const profileId = url.searchParams.get('profileId');
+
+        if (!profileId) {
+            return new NextResponse("Profile ID not provided", { status: 400 });
+        }
 
         const profile = await db.profile.findUnique({
-            where: { id: profileId as string }
+            where: { id: profileId }
         });
 
         if (profile) {
-            res.status(200).json(profile);
+            return NextResponse.json(profile);
         } else {
-            res.status(404).json({ message: 'Profile not found' });
+            return new NextResponse("Profile not found", { status: 404 });
         }
     } catch (error) {
-        console.error("[PROFILES_GET]", error);
-        res.status(500).send('Internal Server Error');
+        console.log("[PROFILES_GET]", error);
+        return new NextResponse("Internal Error", { status: 500 });
     }
 }
