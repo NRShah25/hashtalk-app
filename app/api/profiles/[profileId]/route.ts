44bb38a240906c80562d1fb1aef8119d0ee5b781
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-12-04 16:30:12 -0500

    Update route.ts

--- app/api/profiles/[profileId]/route.ts
+++ app/api/profiles/[profileId]/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/lib/db";
 
-export async function GET(req: { url: string; }) {
+export async function GET(req: NextRequest) {
     try {
         const url = new URL(req.url);
         const profileId = url.searchParams.get('profileId');
@@ -50,4 +50,3 @@ export async function PATCH(
       return new NextResponse("Internal Error", { status: 500 });
     }
   }
-  
