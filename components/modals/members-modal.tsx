e51d99632fb530a999a96aea38ff48105acf4e64
Stefan Miladinović <95955605+sdmila@users.noreply.github.com>
2023-12-11 14:39:00 -0500

    Update members-modal.tsx

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -108,7 +108,7 @@ export const MembersModal = () => {
             <div key={member.id} className="flex items-center gap-x-2 mb-6">
               <UserAvatar src={member.profile.imageUrl} />
               <div className="font-semibold text-s flex flex-col gap-y-1">
-                <div className="text-xs font-semibold flex items-center gap-x-1">
+                <div className="flex items-center gap-x-1">
                   {member.profile.displayName === member.profile.username ? member.profile.displayName : `${member.profile.displayName} (${member.profile.username})`}
                   {roleIconMap[member.role]}
                 </div>
